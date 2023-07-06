97f520396cd97c63ef3be55d238a3afaaad31bd5
Koshila MD <Koshila@mediadefined.local>
2023-07-06 16:54:03 +0530

    API testing

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ app = FastAPI()
 
 class Item(BaseModel):
     name: str
-    price: float
+    price: float = 20.1
     is_offer: Union[bool, None] = None
+    
+class Numbers(BaseModel):
+    name: str
+    first_number: int = 20
+    second_number: int = 30
+    summation: Union[bool, None] = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
@@ -23,3 +28,16 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def update_item(item_id: int, item: Item):
     return {"item_price": item.price, "item_id": item_id}
 
+@app.post("/numbers/{number_id}")
+def post_numbers(fnumber_id: int, snumber_id: int):
+    return {"First Number" : fnumber_id, "Second Number" : snumber_id}
+
+@app.post("/numbers/{number_ids}")
+def sum_numbers(fnumber_id : int, snumber_id : int, numbers : Numbers):
+    return {"Summation of numbers": fnumber_id + snumber_id, "Second Num from class": numbers.second_number }
+
+
+
+
+
+    
